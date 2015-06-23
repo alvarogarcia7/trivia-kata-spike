c9cf13b422f12a4cbb514354419d661ea698d2ae
Alvaro Garcia <derecerca@gmail.com>
2015-06-23 16:55:09 +0200

    create a golden master for the end2end with six different seeds

--- java/src/test/java/com/adaptionsoft/games/trivia/End2EndTest.java
+++ java/src/test/java/com/adaptionsoft/games/trivia/End2EndTest.java
@@ -1,36 +1,46 @@
 
 package com.adaptionsoft.games.trivia;
-import java.util.Random;
 
 import com.adaptionsoft.games.uglytrivia.Game;
+import org.junit.Test;
 
+import java.util.Random;
 
 public class End2EndTest {
 
-	private static boolean notAWinner;
 
-	public static void main(String[] args) {
-		Game aGame = new Game();
-		
-		aGame.add("Chet");
-		aGame.add("Pat");
-		aGame.add("Sue");
-		
-		Random rand = new Random();
-	
-		do {
-			
-			aGame.roll(rand.nextInt(5) + 1);
-			
-			if (rand.nextInt(9) == 7) {
-				notAWinner = aGame.wrongAnswer();
-			} else {
-				notAWinner = aGame.wasCorrectlyAnswered();
-			}
-			
-			
-			
-		} while (notAWinner);
-		
+    @Test
+	public void test_with_seed1(){
+        playGameWithSeed(1L);
+        playGameWithSeed(2L);
+        playGameWithSeed(3L);
+        playGameWithSeed(4L);
+        playGameWithSeed(5L);
+        playGameWithSeed(6L);
 	}
+
+    private void playGameWithSeed(long seed) {
+        boolean notAWinner;
+        Game aGame = new Game();
+
+        aGame.add("Chet");
+        aGame.add("Pat");
+        aGame.add("Sue");
+
+        Random rand = new Random(seed);
+
+        do {
+
+            aGame.roll(rand.nextInt(5) + 1);
+
+            if (rand.nextInt(9) == 7) {
+                notAWinner = aGame.wrongAnswer();
+            } else {
+                notAWinner = aGame.wasCorrectlyAnswered();
+            }
+
+
+
+        } while (notAWinner);
+    }
 }
