340b4a09d5a85883296179a18c4d087b63fe5a20
Alvaro Garcia <derecerca@gmail.com>
2015-06-23 18:16:41 +0200

    save process - uknown state

--- src/test/java/com/adaptionsoft/games/trivia/SomeTest.java
+++ src/test/java/com/adaptionsoft/games/trivia/SomeTest.java
@@ -6,6 +6,8 @@ import static org.junit.Assert.*;
 import com.adaptionsoft.games.uglytrivia.Game;
 import org.junit.Test;
 
+import java.util.Random;
+
 public class SomeTest {
 
 	@Test
@@ -31,6 +33,28 @@ public class SomeTest {
         agame.add("7");
 
         assertThat(agame.howManyPlayers(), is(7));
+
+    }
+
+    @Test
+    public void a_player_that_always_guesses_correct_should_win() throws Exception {
+
+
+
+        boolean notAWinner=true;
+        Game aGame = new Game();
+
+        boolean[] answers = new boolean[]{true, false};
+
+        aGame.add("Chet");
+        aGame.add("Pat");
+
+        assertTrue(aGame.isPlayable());
+
+        for(int i=0;i<6;i++) {
+            notAWinner = aGame.wasCorrectlyAnswered();
+        }
+        assertFalse(notAWinner);
 
     }
 }
