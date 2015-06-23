92ceef6a33006657e1a8bd28526b1c57e02d5fcb
Alvaro Garcia <derecerca@gmail.com>
2015-06-23 17:24:26 +0200

    save process - uknown state

--- src/test/java/com/adaptionsoft/games/trivia/SomeTest.java
+++ src/test/java/com/adaptionsoft/games/trivia/SomeTest.java
@@ -2,13 +2,20 @@ package com.adaptionsoft.games.trivia;
 
 import static org.junit.Assert.*;
 
+import com.adaptionsoft.games.uglytrivia.Game;
 import org.junit.Test;
+
+import java.util.stream.Stream;
 
 public class SomeTest {
 
-	@Test
-	public void true_is_true() throws Exception {
-		assertTrue(false);
+	@Test(expected = IndexOutOfBoundsException.class)
+	public void test_max_players_reached() throws Exception {
+		Game game = new Game();
+
+        for (int i )
+
+        Stream<String>.of("1", "2", "3", "4", "5", "6", "7").;
 
 
 	}
